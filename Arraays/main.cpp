6b65e19aff37d90171857d4bb2153a84221c3d3d
hp <hp@DESKTOP-FT1RJ7V>
2024-02-05 23:38:07 +0500

    introduction to Arriy

--- Arraays/main.cpp
+++ Arraays/main.cpp
@@ -5,4 +5,13 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
+	cout << "Hello Arrays" << endl;
+	const int SIZE = 5;
+	int arr[SIZE] = { 3,5,8 };
+	cout << int() << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
