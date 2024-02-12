f81389321c7cc50a4e4628d5300cbd68a282e4d1
hp <hp@DESKTOP-FT1RJ7V>
2024-02-12 21:03:49 +0500

    arays

--- Arraays/main.cpp
+++ Arraays/main.cpp
@@ -1,17 +1,25 @@
 //Arrays(масcив)
 #include<iostream>
 using namespace std;
+using std::cout;
+using std::endl;
+#define ARRAYS
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Arrays" << endl;
+#ifdef ARRAYS
+	
+	cout << "Hello Arrays"<<endl;
 	const int SIZE = 5;
-	int arr[SIZE] = { 3,5,8 };
+	int arr[SIZE] = { 3,5,8,10,7 };
 	cout << int() << endl;
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+#endif // ARRAYS
+
 }
